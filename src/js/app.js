9df0532beb38f0f17e0e0371b75715f9ac289122
andriyBelash <belasha6@gmail.com>
2022-04-16 10:50:01 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+flsFunctions.menuInit();
+
+flsFunctions.isSwiper();
+
+flsFunctions.isSpoller();
+
+flsFunctions.isDinamicAdaptive();
+
+import './files/tippy.js'
+
+tippy('[data-tippy-content]')
+// let userIcon = document.querySelector('.user-header__icon')
+
+// userIcon.addEventListener('click', () => {
+//     let userMenu = document.querySelector('.user-header__menu');
+//     userMenu.classList.toggle('_active');
+// })
+
+// document.documentElement.addEventListener('click', (e) => {
+//     if(!e.target.closest('.user-header')) {
+//         let userMenu = document.querySelector('.user-header__menu');
+//         userMenu.classList.remove('_active')
+//     }
+// })
+
